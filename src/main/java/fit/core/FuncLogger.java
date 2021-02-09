8d711293f931c30afb7f0a7501b9043f94c78151
alfredojampietrofilho <alfredojfilho@outlook.com>
2021-02-08 21:43:41 -0300

    Criacao da Classe FuncLogger com a função create

--- src/main/java/fit/core/FuncLogger.java
+++ src/main/java/fit/core/FuncLogger.java
@@ -1,5 +1,16 @@
 package fit.core;
 
+import java.util.function.Function;
+
 public class FuncLogger {
+
+    public static Function<String, String> create(String prefixo) {
+        return str -> {
+            var text = String.format("%s %s", prefixo, str);
+            System.out.println(text);
+
+            return text;
+        };   
+    }
     
 }
